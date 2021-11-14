d2fa4bd696bbdb713c499371b440fc553a3ac6da
Pedro Ulhoa <pedroulhoa@live.com>
2021-11-14 12:56:05 -0300

    feat: product entity / repository

---
+++ projects/product-api/src/main/java/com/aws/productapi/entity/Product.java
@@ -0,0 +1,85 @@
+package com.aws.productapi.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "product",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"code"})
+        }
+)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 32, nullable = false)
+    private String name;
+
+    @Column(length = 24, nullable = false)
+    private String model;
+
+    @Column(length = 8, nullable = false)
+    private String code;
+
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ projects/product-api/src/main/java/com/aws/productapi/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.aws.productapi.repository;
+
+import com.aws.productapi.entity.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    Optional<Product> findByCode(String code);
+}
