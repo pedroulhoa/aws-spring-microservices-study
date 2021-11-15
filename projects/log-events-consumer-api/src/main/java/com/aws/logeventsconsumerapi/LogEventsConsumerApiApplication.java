f878d7c0b65a259452a1255df64653173c70f0af
Pedro Ulhoa <pedroulhoa@live.com>
2021-11-15 10:09:57 -0300

    feat: new project log events consumer api

---
+++ projects/log-events-consumer-api/src/main/java/com/aws/logeventsconsumerapi/LogEventsConsumerApiApplication.java
@@ -0,0 +1,13 @@
+package com.aws.logeventsconsumerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogEventsConsumerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogEventsConsumerApiApplication.class, args);
+    }
+
+}
